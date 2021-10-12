95876dea07f17a569d1ff477477d30b95ec15f25
Ankita Makade <78643106+Ankita196@users.noreply.github.com>
2021-10-12 08:41:45 +0000

    done

--- src/components/Anime.js
+++ src/components/Anime.js
@@ -5,7 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
